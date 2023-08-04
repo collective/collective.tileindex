b268c0c159928c31716dae2242cb504d0ec0b878
Fred van Dijk <fredvd@gmail.com>
2023-08-04 13:12:52 +0200

    Add installing the index, fix finding tile_types

--- src/collective/tileindex/indexers/tile_types.py
+++ src/collective/tileindex/indexers/tile_types.py
@@ -3,6 +3,7 @@
 from plone.app.contenttypes.interfaces import IDocument
 from plone.dexterity.interfaces import IDexterityContent
 from plone.indexer import indexer
+from bs4 import BeautifulSoup
 
 
 @indexer(IDexterityContent)
@@ -14,4 +15,22 @@ def dummy(obj):
 @indexer(IDocument)  # ADJUST THIS!
 def tile_types(obj):
     """Calculate and return the value for the indexer"""
-    return obj.tile_types
+
+    # Check if the object has a getLayout method (supports tiles)
+    if hasattr(obj, "getLayout"):
+        # Get the tiles (collection of tiles)
+        content_layout = obj.getLayout()
+        if content_layout:
+            soup = BeautifulSoup(content_layout)
+            divs = soup.find_all(attrs={"data-tile": True})
+            mapping = {}
+            for dt in [d["data-tile"] for d in divs]:
+                items = dt.split("/")
+                if len(items)==3:
+                    mapping[items[2]] = items[1]
+                else:
+                    # This tile has no annotation, so it might be a richtext tile showing the text field    
+                    continue
+            return set(mapping.keys())
+
+    return []
