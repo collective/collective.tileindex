5157ea7d60923c8b629b8fc85061e6a7e0c6e240
Maurits van Rees <maurits@vanrees.org>
2023-11-15 15:49:30 +0100

    Show the tile types when calling the tile_types view.
    Simplified the view and its tests: I never use an interface for this.

--- src/collective/tileindex/tests/test_view_tile_types_test_view.py
+++ src/collective/tileindex/tests/test_view_tile_types_test_view.py
@@ -1,12 +1,8 @@
-# -*- coding: utf-8 -*-
-from collective.tileindex.testing import COLLECTIVE_TILEINDEX_FUNCTIONAL_TESTING
 from collective.tileindex.testing import COLLECTIVE_TILEINDEX_INTEGRATION_TESTING
-from collective.tileindex.views.tile_types_test_view import ITileTypesTestView
 from plone import api
 from plone.app.testing import setRoles
 from plone.app.testing import TEST_USER_ID
 from zope.component import getMultiAdapter
-from zope.interface.interfaces import ComponentLookupError
 
 import unittest
 
@@ -17,31 +13,17 @@ class ViewsIntegrationTest(unittest.TestCase):
     def setUp(self):
         self.portal = self.layer["portal"]
         setRoles(self.portal, TEST_USER_ID, ["Manager"])
-        api.content.create(self.portal, "Folder", "other-folder")
-        api.content.create(self.portal, "Document", "front-page")
 
-    def test_tile_types_is_registered(self):
+    def test_tile_types_is_registered_document(self):
+        api.content.create(self.portal, "Document", "front-page")
+        view = getMultiAdapter(
+            (self.portal["front-page"], self.portal.REQUEST), name="tile-types"
+        )
+        self.assertIsNone(view.tile_types)
+
+    def test_tile_types_is_registered_folder(self):
+        api.content.create(self.portal, "Folder", "other-folder")
         view = getMultiAdapter(
             (self.portal["other-folder"], self.portal.REQUEST), name="tile-types"
         )
-        self.assertTrue(ITileTypesTestView.providedBy(view))
-
-    def test_tile_types_not_matching_interface(self):
-        view_found = True
-        try:
-            view = getMultiAdapter(
-                (self.portal["front-page"], self.portal.REQUEST), name="tile-types"
-            )
-        except ComponentLookupError:
-            view_found = False
-        else:
-            view_found = ITileTypesTestView.providedBy(view)
-        self.assertFalse(view_found)
-
-
-class ViewsFunctionalTest(unittest.TestCase):
-    layer = COLLECTIVE_TILEINDEX_FUNCTIONAL_TESTING
-
-    def setUp(self):
-        self.portal = self.layer["portal"]
-        setRoles(self.portal, TEST_USER_ID, ["Manager"])
+        self.assertIsNone(view.tile_types)
--- src/collective/tileindex/views/tile_types_test_view.py
+++ src/collective/tileindex/views/tile_types_test_view.py
@@ -1,24 +1,12 @@
-# -*- coding: utf-8 -*-
-
-# from collective.tileindex import _
+from collective.tileindex.utils import get_tile_types_from_obj
+from functools import cached_property
 from Products.Five.browser import BrowserView
-from zope.interface import implementer
-from zope.interface import Interface
 
 
-# from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
-
-
-class ITileTypesTestView(Interface):
-    """Marker Interface for ITileTypesTestView"""
-
-
-@implementer(ITileTypesTestView)
 class TileTypesTestView(BrowserView):
-    # If you want to define a template here, please remove the template from
-    # the configure.zcml registration of this view.
-    # template = ViewPageTemplateFile('tile_types_test_view.pt')
-
-    def __call__(self):
-        # Implement your own actions:
-        return self.index()
+    @cached_property
+    def tile_types(self):
+        tiles = get_tile_types_from_obj(self.context)
+        if not tiles:
+            return
+        return sorted(tiles)
